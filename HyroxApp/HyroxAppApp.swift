a56f7c640e625822c8d31662b22c51cb24ed26b6
emiliersrd <emilie.rouinsard@etu.ec-lyon.fr>
2025-12-29 09:50:29 +0100

    Initial Commit

---
+++ HyroxApp/HyroxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyroxAppApp.swift
+//  HyroxApp
+//
+//  Created by emilie rouinsard on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct HyroxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HyroxAppTests/HyroxAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HyroxAppTests.swift
+//  HyroxAppTests
+//
+//  Created by emilie rouinsard on 29/12/2025.
+//
+
+import Testing
+@testable import HyroxApp
+
+struct HyroxAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
